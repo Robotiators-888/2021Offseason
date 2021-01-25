734b5eacf8841339b53b942fcd4d1abc29efa8d2
Spencer <Samcgrath2005@gmail.com>
2021-01-25 15:43:08 -0500

    Update DriveTrain.java

--- src/main/java/frc/robot/subsystems/DriveTrain.java
+++ src/main/java/frc/robot/subsystems/DriveTrain.java
@@ -8,24 +8,38 @@ import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
 public class Robot {
   private DifferentialDrive m_myRobot;
-  private Joystick m_leftStick;
-  private Joystick m_rightStick;
-  private static int leftDeviceID = 1; 
-  private static int rightDeviceID = 2;
-  private CANSparkMax m_leftMotor;
-  private CANSparkMax m_rightMotor;
+  private Joystick m_leftStickPrimary;
+  private Joystick m_leftStickSecondary;
+  private Joystick m_rightStickPrimary;
+  private Joystick m_rightStickSecondary;
+  private static int leftDeviceIDPrimary = 1; 
+  private static int leftDeviceIDSecondary = 2;
+  private static int rightDeviceIDPrimary = 3;
+  private static int rightDeviceIDSecondary = 4; 
+  private CANSparkMax m_leftMotorPrimary;
+  private CANSparkMax m_leftMotorSecondary;
+  private CANSparkMax m_rightMotorPrimary;
+  private CANSparkMax m_rightMotorSecondary;
+/* */
+  
 
-  @override
+@override
   public void robotInit(){
-      m_leftMotor = new CANSparkMax(leftDeviceID, MotorType.kbrushless);
-      m_rightMotor = new CANSparkMax(rightDeviceID, MotorType.kbrushless);
+      m_leftMotorPrimary = new CANSparkMax(leftDeviceIDPrimary, MotorType.kbrushless);
+      m_leftMotorSecondary = new CANSparkMax(leftDeviceIDSecondary, MotorType.kbrushless);
+      m_rightMotorPrimary = new CANSparkMax(rightDeviceIDPrimary, MotorType.kbrushless);
+      m_rightMotorSecondary = new CANSparkMax(rightDeviceIDSecondary, MotorType.kbrushless);
+    /* */
 
+        m_myRobot = new DifferentialDrive(m_leftMotorPrimary, m_leftMotorSecondary, m_rightMotorPrimary,m_rightMotorSecondary);
 
-        m_myRobot = new DifferentialDrive(m_leftMotor, m_rightMotor);
-
-        m_leftStick = new Joystick(0);
-        m_rightStick = new Joystick(1);
+        m_leftStickPrimary = new Joystick(1);
+        m_leftStickSecondary = Joystick(1) * -1;
+        m_rightStickPrimary = new Joystick(2);
+        m_rightStickSecondary = Joystick(2) * -1;
+    
     }
+   
     @override
     public void teleopPeriodic(){
         m_myRobot.tankDrive(m_leftStick.getY(), m_rightStick.getY());
